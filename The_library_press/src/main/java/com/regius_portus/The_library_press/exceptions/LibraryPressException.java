0cd7c312239ace82e87ddd4f02bc8e71c5362440
Abigail Godwin <131200193+Abby-uye@users.noreply.github.com>
2024-06-25 16:19:33 +0100

    Delete The_library_press directory

--- The_library_press/src/main/java/com/regius_portus/The_library_press/exceptions/LibraryPressException.java
+++
@@ -1,7 +0,0 @@
-package com.regius_portus.The_library_press.exceptions;
-
-public class LibraryPressException extends Throwable {
-    public LibraryPressException(String message) {
-        super(message);
-    }
-}
--- The_library_press/src/main/java/com/regius_portus/The_library_press/exceptions/ReaderExistException.java
+++
@@ -1,7 +0,0 @@
-package com.regius_portus.The_library_press.exceptions;
-
-public class ReaderExistException extends Throwable {
-    public ReaderExistException(String message) {
-        super(message);
-    }
-}
